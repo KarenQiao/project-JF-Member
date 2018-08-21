4ecd0dbc8f65961feda69d322a839ae8ad7371a1
KarenQiao <498101094@qq.com>
2018-08-21 14:21:44 +0800

    上传修改的页面

--- src/page/login/forgetPasd.vue
+++ src/page/login/forgetPasd.vue
@@ -4,23 +4,23 @@
     <div class="input_box ">
 
       <label class="member_left_center">
-        <span><img src="../../../static/images/logo1.png"></span>
+        <span><img src="../../../static/images/icon_phone.png"></span>
         <input type="tel" placeholder="请输入电话号码"/>
       </label>
 
       <label class="member_left_center">
-        <span><img src="../../../static/images/logo1.png"></span>
+        <span><img src="../../../static/images/icon_safety.png"></span>
         <input type="tel" placeholder="请输入验证码"/>
-        <button class="getCode locked" id="getCode">获取验证码</button>
+        <button class="getCode" id="getCode">获取验证码</button>
       </label>
 
       <label class="member_left_center">
-        <span><img src="../../../static/images/logo1.png"></span>
+        <span><img src="../../../static/images/icon_locked.png"></span>
         <input type="password" placeholder="请输入薪密码"/>
       </label>
 
       <label class="member_left_center">
-        <span><img src="../../../static/images/logo1.png"></span>
+        <span><img src="../../../static/images/icon_locked.png"></span>
         <input type="tel" placeholder="请再次确认密码"/>
       </label>
 
