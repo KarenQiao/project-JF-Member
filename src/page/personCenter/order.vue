14d70b9728850fe613209dd8f6eb4f6e234dfbe2
KarenQiao <498101094@qq.com>
2018-08-31 15:13:12 +0800

    上传上拉加载

--- src/page/personCenter/order.vue
+++ src/page/personCenter/order.vue
@@ -2,17 +2,17 @@
 
   <div>
 
-  <div class="fixed_top_bar">
+<!--  <div class="fixed_top_bar">
     <div class="bar"></div>
     <div class="all_tab yao_shadow">
-     <!-- <p class="show" onclick="orderFn.tabChange(0)" @click="getList('')">全部</p>-->
+     &lt;!&ndash; <p class="show" onclick="orderFn.tabChange(0)" @click="getList('')">全部</p>&ndash;&gt;
       <p class="show" onclick="orderFn.tabChange(0)" @click="getList('00')">待付款</p>
       <p onclick="orderFn.tabChange(1)" @click="getList('10')">已付款</p>
       <p onclick="orderFn.tabChange(2)" @click="getList('20')">已发货</p>
       <p onclick="orderFn.tabChange(3)" @click="getList('30')">已过期</p>
       <p onclick="orderFn.tabChange(4)" @click="getList('40')">已完成</p>
     </div>
-  </div>
+  </div>-->
 
   <div class="my_order_page">
 
@@ -21,238 +21,33 @@
       <!--全部-->
       <div class="all box">
 
-        <a class="order_list" v-for="item in dataList">
-          <div class="order_status member_between_center">
-            <p>
-              <span>订单号:</span>
-              <span>{{item.orderNo}}</span>
-            </p>
-            <p class="golden_text">{{item.orderStatus|orderStatue}}</p>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
+        <scroller :on-infinite="infinite" ref:my_scroller  delegate-id="myScroller">
+          <a class="order_list" v-for="item in dataList">
+            <div class="order_status member_between_center">
+              <p>
+                <span>订单号:</span>
+                <span>{{item.orderNo}}</span>
+              </p>
+              <p class="golden_text">待发货</p>
             </div>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
+            <div v-for="list in item.detailList" class="details member_left_center" >
 
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
+              <p><img :src="list.shopUrl"></p>
+              <div>
+                <span class="title">{{list.shopTitle}}</span>
+                <span>{{list.shopSpec}}</span>
 
+                <p class="member_between_center"><span class="golden_text">￥{{list.shopPrice}}</span><span>X{{list.shopNum}}</span></p>
+
+              </div>
             </div>
-          </div>
-          <div class="total">
-            总共<span>{{item.shopNum}}</span>件商品，合计<span>￥800.00</span> 包含运费<span>￥{{item.extraMoney}}</span>
-          </div>
-        </a>
 
-        <a class="order_list">
-          <div class="order_status member_between_center">
-            <p>
-              <img src="../../../static/images/ent_logo.png">
-              <span>德开药店</span>
-            </p>
-            <p class="golden_text">已完成</p>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
+            <div class="total">
+              总共<span>{{item.shopNum}}</span>件商品，合计<span>￥800.00</span> 包含运费<span>￥{{item.extraMoney}}</span>
             </div>
-          </div>
-          <div class="total">
-            总共<span>1</span>件商品，合计<span>￥600.00</span> 包含运费<span>￥10.00</span>
-          </div>
-        </a>
-
-      </div>
-
-      <!--代付款-->
-      <div class="paying box">
-
-        <a class="order_list">
-          <div class="order_status member_between_center">
-            <p>
-              <img src="../../../static/images/ent_logo.png">
-              <span>德开药店</span>
-            </p>
-            <p class="golden_text">待付款</p>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="total">
-            总共<span>1</span>件商品，合计<span>￥600.00</span> 包含运费<span>￥10.00</span>
-          </div>
-        </a>
-
-        <a class="order_list">
-          <div class="order_status member_between_center">
-            <p>
-              <img src="../../../static/images/ent_logo.png">
-              <span>德开药店</span>
-            </p>
-            <p class="golden_text">待付款</p>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="total">
-            总共<span>1</span>件商品，合计<span>￥600.00</span> 包含运费<span>￥10.00</span>
-          </div>
-        </a>
-      </div>
-
-
-      <!--待收货-->
-      <div class="evaluate box">
-
-        <a class="order_list">
-          <div class="order_status member_between_center">
-            <p>
-              <img src="../../../static/images/ent_logo.png">
-              <span>德开药店</span>
-            </p>
-            <p class="golden_text">已完成</p>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="total">
-            总共<span>1</span>件商品，合计<span>￥600.00</span> 包含运费<span>￥10.00</span>
-          </div>
-        </a>
-
-      </div>
-
-
-      <!--已完成-->
-      <div class="evaluate box">
-
-        <a class="order_list">
-          <div class="order_status member_between_center">
-            <p>
-              <img src="../../../static/images/ent_logo.png">
-              <span>德开药店</span>
-            </p>
-            <p class="golden_text">已完成</p>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="total">
-            总共<span>1</span>件商品，合计<span>￥600.00</span> 包含运费<span>￥10.00</span>
-          </div>
-        </a>
-
-      </div>
-
-      <!--已取消-->
-      <div class="evaluate box">
-
-        <a class="order_list">
-          <div class="order_status member_between_center">
-            <p>
-              <img src="../../../static/images/ent_logo.png">
-              <span>德开药店</span>
-            </p>
-            <p class="golden_text">已完成</p>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="details member_left_center">
-            <p><img src="../../../static/images/produce_2.png"></p>
-            <div>
-              <span class="title">kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器kindle电子阅读器</span>
-              <span>白色，12寸</span>
-
-              <p class="member_between_center"><span class="golden_text">￥500</span><span>X1</span></p>
-
-            </div>
-          </div>
-          <div class="total">
-            总共<span>1</span>件商品，合计<span>￥600.00</span> 包含运费<span>￥10.00</span>
-          </div>
-        </a>
-
+          </a>
+        </scroller>
+        <p v-show="noData">没有更多数据了~</p>
       </div>
 
     </div>
@@ -269,19 +64,29 @@
 
   import API from '../../assets/api'
 
+  import Vue from 'vue'
+  import VueScroller from 'vue-scroller'
+  Vue.use(VueScroller)
+
   export default {
     name:'order',
 
     data(){
       return{
 
-        dataList:'',
+        dataList:[],
 
         userId:'',
 
-        orderStatus:'',//00-待付款,10-已付款,20-已发货,30-已过期,40-已完成,全部为空
+        orderStatus:'3',//00-待付款,10-已付款,20-已发货,30-已过期,40-已完成,全部为空
 
         page:1,
+
+        params:{},
+
+        noData:false,
+
+
 
       }
     },
@@ -292,29 +97,29 @@
 
       this.userId=userData.userId;
 
-      this.getList('');
+      this.getList(this.page);
     },
 
     methods:{
 
       getList(num){
 
-        let params={
-          userId:this.userId,
-
-          orderStatus:num,
-
-          page:this.page
-
-        }
-
-        API.postFn(API.order,params).then(function (res) {
+        API.postFn(API.order,{userId:this.userId, orderStatus:this.orderStatus,page:num}).then(function (res) {
 
           console.log(res)
 
           if(res.data.code='00000'){
 
-            this.dataList=res.data.list;
+            for(var i=0; i<res.data.list.length;i++){
+
+              this.dataList.push(res.data.list[i])
+
+            }
+
+            if(res.data.list.length<10){
+
+              this.noData = "没有更多数据"
+            }
 
           }else {
             jfShowTips.toastShow({'text':res.data.message})
@@ -325,10 +130,35 @@
 
           }.bind(this))
 
+      },
+
+      infinite(done) {
+
+        if(this.noData){
+
+          setTimeout(() => {
+            this.$refs.my_scroller.finishInfinite(2);
+          });
+          return false;
+        }
+
+        let self = this;//this指向问题
+        setTimeout(() => {
+          self.page+=1;
+          self.getList(self.page);
+
+          done()
+
+        }, 1500)
       }
 
+      },
+
+//done()表示这次异步加载数据完成，加载下一次
+//因为这个是同步的，加了setTimeout就是异步加载数据；
+//因为涉及到this指向问题，所以将他放在一个变量里。
 
 
-    }
   }
 </script>
+
