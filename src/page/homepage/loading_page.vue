7a21f9da6e767267a834e5fa5f241005db282b54
CNMneuropathy <15221630549@163.com>
2018-10-11 19:21:03 +0800

    新增电子贺卡，启动页
    新增电子贺卡，启动页

---
+++ src/page/homepage/loading_page.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <div class="loading_background">
+    <div class="loading_center">
+      <div class="loading_page_logo"><img src="../../../static/images/img_start_logo.png"/></div>
+
+      <p class="loading_font">WELCOME TO JIA CLUB</p>
+    </div>
+  </div>
+
+</template>
